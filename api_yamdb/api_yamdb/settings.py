485d04af3229f76db799206f696b93c7fa5f7bb8
Nick Prokhorov <irider.pro@yandex.ru>
2022-06-27 23:20:50 +0300

    fix reviewer2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -15,7 +15,7 @@ DEBUG = False
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split()
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
 AUTH_USER_MODEL = "users.User"
 
 # Application definition
